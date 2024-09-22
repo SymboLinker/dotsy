d979eca8e873c88eba5a4e7bff002bee56a25f94
Symbolinker <symbolinker@gmail.com>
2024-09-22 06:59:43 +0200

    Fix English spelling mistake

--- src/internal/getDotCoordinateInfos.ts
+++ src/internal/getDotCoordinateInfos.ts
@@ -8,11 +8,11 @@ export function getDotCoordinateInfos(dots: Dot[], coordinates: Coordinate[]) {
     const dotCoordinateInfos: DotCoordinateInfo[] = [];
     for (const dot of dots) {
         const radius = dot.radius;
-        const eligableCoordinates = coordinates.filter(x => x.distance > radius);
-        if (eligableCoordinates.length == 0) {
+        const eligibleCoordinates = coordinates.filter(x => x.distance > radius);
+        if (eligibleCoordinates.length == 0) {
             throw "The area is too small (or the size of the dots too big).";
         }
-        const chosenCoordinate = random(eligableCoordinates);
+        const chosenCoordinate = random(eligibleCoordinates);
         dotCoordinateInfos.push(new DotCoordinateInfo(dot, chosenCoordinate));
 
         for (const coordinate of coordinates) {
